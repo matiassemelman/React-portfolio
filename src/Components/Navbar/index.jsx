b8eeef8e6e268269b7fbc7123ab887648b77ed47
maatii95 <matias.semelman@protonmail.com>
2022-02-13 17:42:32 -0300

    Changing font size

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,16 +1,16 @@
 export const Navbar = () => {
   return (
-    <div className="flex w-screen justify-between p-2">
+    <div className="flex w-screen justify-between p-4 sm:p-24">
       <h1 className="flex-none">Logo</h1>
-      <ul className="flex">
-        <li className="pr-2">Projects</li>
-        <li className="pr-2">About</li>
-        <li className="pr-2">Contact</li>
+      <ul className="flex sm:text-2xl">
+        <li className="pr-2 sm:pr-10">Projects</li>
+        <li className="pr-2 sm:pr-10">About</li>
+        <li className="pr-2 sm:pr-10">Contact</li>
 
         {/* Menu Icon from https://heroicons.com/*/}
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
+          className="h-6 w-6 sm:h-8 sm:w-8"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
