e11a4e833d42ce86eadfd25dd0f3f64ebc6f4f27
maatii95 <matias.semelman@protonmail.com>
2022-02-14 22:09:29 -0300

    Fix

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -2,11 +2,7 @@ export const Navbar = () => {
   return (
     <header className="flex bg-zinc-800 w-screen justify-between px-4 py-4 fixed">
       <div>
-        <img
-          src="https://imgur.com/KJLghxT"
-          alt="My Logo"
-          className="flex-none w-10 h-10"
-        />
+        <img src="" alt="" className="flex-none" />
       </div>
 
       <ul className="flex text-lg">
