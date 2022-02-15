9060f07185ec0f74824bc0d53eb0ff411eb71a1f
maatii95 <matias.semelman@protonmail.com>
2022-02-14 22:14:02 -0300

    Logo added

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -2,12 +2,16 @@ export const Navbar = () => {
   return (
     <header className="flex bg-zinc-800 w-screen justify-between px-4 py-4 fixed">
       <div>
-        <img src="" alt="" className="flex-none" />
+        <img
+          src="https://imgur.com/KJLghxT"
+          alt="My Logo"
+          className="flex-none"
+        />
       </div>
 
       <ul className="flex text-lg">
-        <li className="pr-2 ">
-          <a href="#me">About</a>
+        <li className="pr-2">
+          <a href="#">About</a>
         </li>
         <li className="pr-2 ">
           <a href="#">Roadmap</a>
