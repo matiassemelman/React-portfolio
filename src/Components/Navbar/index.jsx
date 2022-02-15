d8d28984005aaffaab9dfd88c7808e84b628190f
Matias Semelman <matias.semelman@protonmail.com>
2022-02-15 10:05:04 -0300

    Update index.jsx

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,13 +1,13 @@
 export const Navbar = () => {
   return (
     <header className="flex bg-zinc-800 w-screen justify-between px-4 py-4 fixed">
-      <div>
-        <img
+      {/* <div>
+    <img
           src="https://imgur.com/KJLghxT"
           alt="My Logo"
           className="flex-none h-8 w-8"
         />
-      </div>
+      </div> */}
 
       <ul className="flex text-lg">
         <li className="pr-2">
